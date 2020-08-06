6cff8a804c1acf6b7aaa45357cf6bbce9f20f6c5
Denis Belov <mirzaf4eg1@gmail.com>
2020-08-06 10:29:16 +0300

    commit 1

---
+++ AdditionalTask/additiona-task/src/main/java/by/epam/learn/mirzaf4eg/App.java
@@ -0,0 +1,13 @@
+package by.epam.learn.mirzaf4eg;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
